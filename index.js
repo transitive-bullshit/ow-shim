43afc49d8cda927e9d4141c90255548b91ab90c4
Travis Fischer <fisch0920@gmail.com>
2018-06-13 17:42:50 -0400

    implement lite version of ow with ES6 Proxy; extremely lightweight

--- index.js
+++ index.js
@@ -1,8 +1,170 @@
 'use strict'
 
-const ow = new Proxy(function () { }, {
-  get: (obj, prop) => ow,
-  apply: () => ow
-})
+// ow(lambda, ow.number.positive)
+// ow.number.positive[validate](lambda)
 
-module.exports = ow
+const func = Symbol('func')
+const validate = Symbol('validate')
+
+const numberPredicates = {
+  positive: (value) => (value > 0),
+  negative: (value) => (value < 0),
+  nonNegative: (value) => (value >= 0),
+  integer: (value) => (value === (value | 0)),
+
+  [func]: {
+    is: (fn) => fn,
+    eq: (v) => (value) => (value === v),
+    gt: (v) => (value) => (value > v),
+    gte: (v) => (value) => (value >= v),
+    lt: (v) => (value) => (value < v),
+    lte: (v) => (value) => (value <= v)
+  }
+}
+
+const stringPredicates = {
+  empty: (value) => (value === ''),
+  nonEmpty: (value) => (value !== ''),
+
+  [func]: {
+    is: (fn) => fn,
+    eq: (v) => (value) => (value === v),
+    length: (v) => (value) => (value.length === v),
+    minLength: (v) => (value) => (value.length >= v),
+    maxLength: (v) => (value) => (value.length <= v),
+    matches: (v) => (value) => v.test(value),
+    startsWith: (v) => (value) => value.startsWith(v),
+    endsWith: (v) => (value) => value.endsWith(v)
+  }
+}
+
+const objectPredicates = {
+  plain: (value) => {
+    if (typeof value !== 'object') return false
+
+    const proto = Object.getPrototypeOf(value)
+    return proto === null || proto === Object.getPrototypeOf({})
+  },
+  empty: (value) => Object.keys(value).length === 0,
+  nonEmpty: (value) => Object.keys(value).length > 0,
+
+  [func]: {
+    is: (fn) => fn,
+    instanceOf: (v) => (value) => (value instanceof v)
+  }
+}
+
+const typePredicates = {
+  number: {
+    predicates: numberPredicates,
+    validator: (value) => {
+      return typeof value === 'number'
+    }
+  },
+  string: {
+    predicates: stringPredicates,
+    validator: (value) => {
+      return typeof value === 'string'
+    }
+  },
+  object: {
+    predicates: objectPredicates,
+    validator: (value) => {
+      return typeof value === 'object'
+    }
+  }
+}
+
+const createOw = ({
+  validators = [],
+  predicates = typePredicates,
+  type = null
+} = { }) => {
+  const ow = new Proxy(function () { }, {
+    get: (obj, key) => {
+      if (key === validate) {
+        return (value, label = 'argument') => {
+          if (!type) {
+            return new Error('missing required type specifier')
+          }
+
+          for (let i = 0; i < validators.length; ++i) {
+            const validator = validators[i]
+            const result = validator.fn(value)
+
+            if (!result) {
+              if (i === 0) {
+                throw new Error(`Expected ${label} \`${value}\` to be of type \`${type}\`, but received type \`${typeof value}\``)
+              } else {
+                throw new Error(`Expected ${type} \`${label}\` \`${value}\` failed predicate \`${validator.key}\``)
+              }
+            }
+          }
+        }
+      }
+
+      const predicate = predicates[key]
+
+      if (predicate) {
+        if (typeof predicate === 'function') {
+          validators.push({
+            key,
+            fn: predicate
+          })
+
+          return ow
+        } else {
+          return createOw({
+            type: key,
+            validators: [
+              {
+                key,
+                fn: predicate.validator
+              }
+            ],
+            predicates: predicate.predicates
+          })
+        }
+      } else {
+        const fn = predicates[func] && predicates[func][key]
+
+        if (fn) {
+          return new Proxy(function () { }, {
+            get: () => {
+              throw new Error(`invalid use of functional predicate "${key}"`)
+            },
+
+            apply: (obj, thisArg, args) => {
+              validators.push({
+                key,
+                fn: fn(...args)
+              })
+
+              return ow
+            }
+          })
+        } else {
+          if (key === 'default' || key === '__esModule') {
+            return ow
+          }
+
+          console.log(`unrecognized predicate "${key}"`)
+          return ow
+          // throw new Error(`unrecognized predicate "${key}"`)
+        }
+      }
+    },
+
+    apply: (obj, thisArg, args) => {
+      if (args.length !== 2 && args.length !== 3) {
+        throw new Error('invalid number of arguments to "ow"')
+      } else {
+        args[1][validate](args[0], args[2])
+      }
+    }
+  })
+
+  return ow
+}
+
+module.exports = createOw()
